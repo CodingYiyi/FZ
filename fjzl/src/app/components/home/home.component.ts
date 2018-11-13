980a857fb19adadb89c786fd841110e0ef23cd1f
“FightYIYI“ <756663138@qq.com>
2018-11-13 16:58:50 +0800

    test

--- fjzl/src/app/components/home/home.component.ts
+++ fjzl/src/app/components/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { map, mergeMap } from 'rxjs/operators';
+import { NzMessageService } from 'ng-zorro-antd';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -9,15 +11,15 @@ import { map, mergeMap } from 'rxjs/operators';
 })
 export class HomeComponent implements OnInit {
 
-  searchInfo:any = {};
-  userInfo :any = {
+  searchInfo: any = {};
+  userInfo: any = {
     idType: "0",
     ageType: "0"
   };
-  patientMedicalInfo:any = {};
-  outOrgInfo:any = {};
-  inOrgInfo:any = {};
-  transferDetail:any = {};
+  patientMedicalInfo: any = {};
+  outOrgInfo: any = {};
+  inOrgInfo: any = {};
+  transferDetail: any = {};
 
   transferOptions = [
     {
@@ -68,69 +70,102 @@ export class HomeComponent implements OnInit {
   disabled = true;
   confirmText = "确认"
   loading = false;
-  constructor(public route: ActivatedRoute, private http: HttpClient) {
+  constructor(
+    public route: ActivatedRoute,
+    private http: HttpClient,
+    private message: NzMessageService
+    ) {
   }
 
   ngOnInit() {
     this.route.queryParams.subscribe(param => {
       // 判断来源（his免登陆 OR login页面）
-      if(typeof param == "object" && Object.keys(param).length > 0){
+      if (typeof param == "object" && Object.keys(param).length > 0) {
         this.fromLogin = false;
         this.disabled = false;
         this.confirmText = "提交"
-      } 
-      this.userInfo = {
-        patientNo:param.patientId,
-        userID:param.idCardNo,
-        patientName:param.patientName,
-        age:param.age,
-        sex:param.gender,
-        birthday:param.birthday,
-        telephone:param.phoneNumber,
-        address:param.address,
-        contactName:param.contactPerson,
-        contactPhone:param.contactPhone,
-        from:param.patientSource,
-        hospitalNo:param.inHospitalNo,
-        clinicNo:param.clinicNo,
-        idType: "0",
-        ageType: "0"
-      }
-      this.patientMedicalInfo={
-        diagnose:param.diagnosis,
-        description:param.description,
-        summary:param.outSummary,
-        costType:param.costType,
-        history:param.illnessHistory
-      },
-      this.inOrgInfo = {
-        direction:"2"
+        this.userInfo = {
+          patientNo: param.patientId,
+          userID: param.idCardNo,
+          patientName: param.patientName,
+          age: param.age,
+          sex: param.gender,
+          birthday: param.birthday,
+          telephone: param.phoneNumber,
+          address: param.address,
+          contactName: param.contactPerson,
+          contactPhone: param.contactPhone,
+          from: param.patientSource,
+          hospitalNo: param.inHospitalNo,
+          clinicNo: param.clinicNo,
+          idType: "0",
+          ageType: "0"
+        }
+        this.patientMedicalInfo = {
+          diagnose: param.diagnosis,
+          description: param.description,
+          summary: param.outSummary,
+          costType: param.costType,
+          history: param.illnessHistory
+        },
+          this.inOrgInfo = {
+            direction: "2"
+          }
+      } else {
+        this.userInfo = JSON.parse(window.localStorage.getItem("userInfo")) || {};
+        this.patientMedicalInfo = JSON.parse(window.localStorage.getItem("patientMedicalInfo")) || {};
+        // this.outOrgInfo = JSON.parse(window.localStorage.getItem("outOrgInfo"));
+        this.inOrgInfo = JSON.parse(window.localStorage.getItem("inOrgInfo")) || {};
       }
     });
   }
 
-  clear(){
-    if(!this.disabled){
-      this.patientMedicalInfo={};
-      this.outOrgInfo = {};
-      this.inOrgInfo={};
+  clear() {
+    if (!this.disabled) {
+      this.userInfo = {};
+      this.patientMedicalInfo = {};
+      this.inOrgInfo = {};
     }
   }
-  submit(){
-    this.http.post<any>("",{
-
-    }).pipe(
-      mergeMap(token=>{
-        return this.http.post("",{})
-      })
-    ).subscribe(res=>{
-      console.log("success");
-    },error=>{
-      console.log("error");
-    },()=>{
-      console.log("complete");
-    })
+  submit() {
     this.loading = true;
+    if (this.fromLogin) {
+      this.http.post<any>("http://172.25.40.18:8089/commit/his", {
+        patientName: "hehe",
+        idCardNo: "341622199501070922",
+        inDeptCode: "13409",
+        inDeptName: "妇保科",
+        inDoctorCode: "lt502",
+        inDoctorName: "靳爱军"
+      })
+        .subscribe(res => {
+          this.loading = false;
+          this.message.create("success", `数据保存成功!!!`);
+          this.userInfo = {};
+          this.patientMedicalInfo = {};
+          this.inOrgInfo = {};
+        }, error => {
+          this.loading = false;
+          this.message.create("error", `数据保存失败...`);
+        }, () => {
+        });
+    } else {
+      window.localStorage.setItem("userInfo", JSON.stringify(this.userInfo));//患者基本信息
+      window.localStorage.setItem("patientMedicalInfo", JSON.stringify(this.patientMedicalInfo));//患者基本医疗信息
+      // window.localStorage.setItem("outOrgInfo", JSON.stringify(this.outOrgInfo));//转出医疗机构信息
+      window.localStorage.setItem("inOrgInfo", JSON.stringify(this.inOrgInfo));//转入医疗机构信息
+      setTimeout(() => {
+        this.loading = false;
+        this.userInfo = {};
+        this.patientMedicalInfo = {};
+        this.inOrgInfo = {};
+        this.message.create("success", `数据保存成功!!!`);
+      }, 3000);
+    }
+  }
+
+  onChange() {
+
   }
 
 }
--- fjzl/src/app/components/login/login.component.ts
+++ fjzl/src/app/components/login/login.component.ts
@@ -22,36 +22,38 @@ export class LoginComponent implements OnInit {
       this.validateForm.controls[i].updateValueAndValidity();
     }
     if (this.validateForm.valid) {
-      this.route.navigate(["/home"], {
-        queryParams: {
-          patientId: "123456",
-          idCardNo: "370685199201010000",
-          patientName: "李四",
-          phoneNumber: "15002951657",
-          patientSource: "1",
-          inHospitalNo: "666",
-          clinicNo: "666",
-          admissionTimes: "",
-          cardNo: "",
-          gender: 1,
-          age: 45,
-          birthday: "1966/10/10",
-          address: "家庭地址测试",
-          costType: "2",
-          diagnosis: "初步诊断测试文字001；初步诊断测试文字002",
-          description: "病情详细描述测试文字",
-          outSummary: "出院小结测试文字。",
-          birthPlaceProv: "",
-          birthPlaceArea: "",
-          postcode: "720000",
-          countryCode: "0513",
-          nationCode: "00001",
-          careerCode: "0002",
-          contactPerson: "张三",
-          contactPhone: "15012345678",
-          illnessHistory: "暂无既往史"
-        }
-      });
+      this.route.navigate(["/home"],
+        // {
+        //   queryParams: {
+        //     patientId: "123456",
+        //     idCardNo: "370685199201010000",
+        //     patientName: "李四",
+        //     phoneNumber: "15002951657",
+        //     patientSource: "1",
+        //     inHospitalNo: "666",
+        //     clinicNo: "666",
+        //     admissionTimes: "",
+        //     cardNo: "",
+        //     gender: 1,
+        //     age: 45,
+        //     birthday: "1966/10/10",
+        //     address: "家庭地址测试",
+        //     costType: "2",
+        //     diagnosis: "初步诊断测试文字001；初步诊断测试文字002",
+        //     description: "病情详细描述测试文字",
+        //     outSummary: "出院小结测试文字。",
+        //     birthPlaceProv: "",
+        //     birthPlaceArea: "",
+        //     postcode: "720000",
+        //     countryCode: "0513",
+        //     nationCode: "00001",
+        //     careerCode: "0002",
+        //     contactPerson: "张三",
+        //     contactPhone: "15012345678",
+        //     illnessHistory: "暂无既往史"
+        //   }
+        // }
+      );
     }
   }
   constructor(private fb: FormBuilder, private route: Router) { }
